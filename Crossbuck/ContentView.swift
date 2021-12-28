0b0c294aba678e19e75a344f890f00cb4c30a607
Nick22morris <nick88morris22@gmail.com>
2021-12-28 17:21:44 -0600

    Initial Commit

---
+++ Crossbuck/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Crossbuck
+//
+//  Created by Other user on 12/28/21.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Crossbuck/CrossbuckApp.swift
@@ -0,0 +1,17 @@
+//
+//  CrossbuckApp.swift
+//  Crossbuck
+//
+//  Created by Other user on 12/28/21.
+//
+
+import SwiftUI
+
+@main
+struct CrossbuckApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
